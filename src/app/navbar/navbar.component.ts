0cd6bded7e1af2aafc8853b64bde82f3555f3e4d
wakil1 <59300535+wakil1@users.noreply.github.com>
2024-07-28 15:41:44 -0400

    added login and logout functionality in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from '../sso-config';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  name: string = "";
+  constructor(private oauthService: OAuthService){
+
+  }
+  ngOnInit(): void {
+    this.configureSingleSignOn();
+    const userClaims: any = this.oauthService.getIdentityClaims();
+    this.name = userClaims.name ? userClaims.name: "";
+
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oauthService.initCodeFlow();
+  }
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  get token() {
+    let claims: any = this.oauthService.getIdentityClaims();
+
+    console.log("NAME IS FSADSADSA\n\n\n")
+    console.log(claims.name)
+    return claims ? claims: null;
+  }
+  
 
 }
